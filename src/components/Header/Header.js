09538d30f9d6753aa0a65f0c8ffa203f9dd250b1
yingsongsana <yingsongsana@gmail.com>
2019-12-03 14:52:11 -0500

    Added dropdown on nav bar
    Dropdown has auth-related items (change password & sign-out)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,7 @@
 import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
+import Nav, { NavItem, NavLink } from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-
-const authenticatedOptions = (
-  <Fragment>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-  </Fragment>
-)
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const unauthenticatedOptions = (
   <Fragment>
@@ -25,14 +19,26 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      Asking For A Friend
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
-        { user ? authenticatedOptions : unauthenticatedOptions }
+        { user && <span className="navbar-text mr-2">Welcome,
+          <Dropdown
+            alignRight
+            title="Dropdown right"
+            id="dropdown-menu-align-right"
+            as={NavItem}>
+            <Dropdown.Toggle as={NavLink}>{user.username}</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#change-password">Change Password</Dropdown.Item>
+              <Dropdown.Item href="#sign-out">Sign Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </span>}
+        { !user && unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
   </Navbar>
