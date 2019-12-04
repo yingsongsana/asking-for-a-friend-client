5902376f0b82996e842cb1f63a10340122507f8d
yingsongsana <yingsongsana@gmail.com>
2019-12-04 09:15:54 -0500

    Created GET request for indexing all posts

---
+++ src/components/shared/Posts.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+const Posts = props => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios(`${apiUrl}/posts`)
+      .then(res => setPosts(res.data.posts))
+      .then(() => props.alert({ heading: 'Success',
+        message: 'Happy browsing!',
+        variant: 'success' }))
+      .catch(console.error)
+  }, [])
+
+  const postsJsx = posts.map(post => (
+    // `_id` because we're using MongoDb here
+    <ListGroup.Item key={post._id} as={'a'} href={`#posts/${post._id}`}>
+      {post.question}
+      {post.description}
+    </ListGroup.Item>
+  ))
+
+  return (
+    <div>
+      <ListGroup>{postsJsx}</ListGroup>
+    </div>
+  )
+}
+
+export default Posts
