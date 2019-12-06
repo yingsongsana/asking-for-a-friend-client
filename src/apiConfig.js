0721042150936b437ce67db7f1bcd02265c36d37
yingsongsana <yingsongsana@gmail.com>
2019-12-06 00:13:11 -0500

    Trying to fix deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-island-25130.herokuapp.com/',
+  production: 'https://boiling-island-25130.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
